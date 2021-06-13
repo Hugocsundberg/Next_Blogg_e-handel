b2f2d24fade91735e4779f25c4d0334d7adc8477
Hugo Sundberg <55212646+Hugocsundberg@users.noreply.github.com>
2021-06-13 23:20:51 +0200

    Working sanity fetch

---
+++ client.ts
@@ -0,0 +1,9 @@
+const sanityClient = require('@sanity/client')
+const client = sanityClient({
+  projectId: '9r33i0al',
+  dataset: 'production',
+  apiVersion: '2021-06-13', // use current UTC date - see "specifying API version"!
+  token: '', // or leave blank for unauthenticated usage
+  useCdn: true, // `false` if you want to ensure fresh data
+})
+export default client
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
+import client from '../client'
+import { useState, useEffect } from 'react'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    console.log('hello')
+    const query = '*[_type == "post"]'
+    client.fetch(query)
+    .then(data => console.log(data))
+  }, []);
+
+  return <Component  {...pageProps} />
 }
 
 export default MyApp
