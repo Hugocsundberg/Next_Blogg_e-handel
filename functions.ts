6415e75c2e27db8a1e241e9eddbd2cd06849929a
Hugo Sundberg <55212646+Hugocsundberg@users.noreply.github.com>
2021-07-28 14:20:43 +0200

    Add localstorage functions

--- functions.ts
+++ functions.ts
@@ -1,4 +1,5 @@
-import { cols } from "./generalTypes"
+import { exception } from "console"
+import { cols, Product } from "./generalTypes"
 import { screenSizes } from "./styles/globalStyleVariables"
 
 export const handleScroll = () => {
@@ -12,4 +13,22 @@ export const getCols = (screenWidth:number): cols => {
   if(screenWidth < S) return 1
   if(screenWidth < M) return 2
   return 3
+}
+
+export const addObjectToStorage = (key:string, data: object) : void => {
+  if(process.browser) {
+    const event = new Event(`add${key}`)
+    const storageData:Array<Object> = JSON.parse(window.localStorage.getItem(key) || '[]')
+    storageData.push(data)
+    window.localStorage.setItem(key, JSON.stringify(storageData))
+    window.dispatchEvent(event)
+  }
+}
+
+export const getFromStorage = (key:string) : (Array<Product> | null | undefined) => {
+  if(process.browser) {
+    const data:string | null = window.localStorage.getItem(key)
+    if( data == null) return data
+    return JSON.parse(data)
+  }
 }
