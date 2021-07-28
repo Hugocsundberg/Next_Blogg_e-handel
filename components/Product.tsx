fa980f6b2ba9763b0eadfdf88156a37855f87f9f
Hugo Sundberg <55212646+Hugocsundberg@users.noreply.github.com>
2021-07-28 23:14:12 +0200

    Add option to remove margin on product

--- components/Product.tsx
+++ components/Product.tsx
@@ -9,21 +9,22 @@ import { margin } from '../styles/globalStyleVariables';
 
 const builder = imageUrlBuilder(client)
 
-const Border = styled.div`
+const Border = styled.div<{removeMargin: boolean}>`
     background: black;
-    padding: 1.5%;
-    margin-bottom: ${margin}rem;
+    padding: 2%;
+    margin-bottom: ${props => props.removeMargin ? '0px' : `${margin}rem`};
     cursor: pointer;
+    box-shadow: 6px 6px 15px -5px #272727c4;
 `
 
 const urlFor = (source: string) => {
   return builder.image(source)
 }
 
-export const Product = ({alt, images, slug}:{alt:string, images: Array<ImageHW>, slug:string}) => {
+export const Product = ({alt, images, slug, removeMargin = false}:{alt:string, images: Array<ImageHW>, slug:string, removeMargin?: boolean}) => {
     return (
         <Link href={`/product/${slug}`}>
-            <Border>
+            <Border removeMargin={removeMargin}>
                 <Image
                     src={urlFor(images[0].asset._ref).url() || '/noImage.jpg'}
                     alt={alt}
