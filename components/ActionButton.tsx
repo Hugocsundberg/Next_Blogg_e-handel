5cffd2d8058e5addc678fc26ab84d3ec4b935e41
Hugo Sundberg <55212646+Hugocsundberg@users.noreply.github.com>
2021-07-29 20:59:34 +0200

    fix spacers

--- components/ActionButton.tsx
+++ components/ActionButton.tsx
@@ -5,14 +5,22 @@ import { blurColor, blurPx, navHeight } from '../styles/globalStyleVariables';
 import { Background, Button, Text } from './GlobalElements/ActionButtonElements';
 
 const Spacer = styled.div`
-    height: ${navHeight}rem;
     position: relative;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    pointer-events: none;   
+    bottom: 0;
+    opacity: 0;
 `
 
 const ActionButton = ({ text, onClick }: {text:string, onClick:any}) => {
     return (
         <>
-        <Spacer></Spacer>
+        <Spacer>
+            <Button color="white" bgcolor="black">S</Button>
+        </Spacer>
         <Background onClick={onClick}>
             <Button color="white" bgcolor="black">{text}</Button>
         </Background>
--- pages/cart/components/ActionButtonCart.tsx
+++ pages/cart/components/ActionButtonCart.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Background, Text, Button } from '../../../components/GlobalElements/ActionButtonElements';
-import { darkGray } from '../../../styles/globalStyleVariables';
+import { darkGray, navHeight } from '../../../styles/globalStyleVariables';
 import styled from 'styled-components';
 
 const H = styled.p`
@@ -16,14 +16,33 @@ const P = styled.p`
     color: ${darkGray};
 `
 
+const Spacer = styled.div`
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    pointer-events: none;   
+    bottom: 0;
+    opacity: 0;
+`
+
 const ActionButtonCart = ({price, tax}: {price: number, tax: number}) => {
     return (
+        <>
+        <Spacer>
+        <H>Total</H>
+            <P>S</P>
+            <P>S</P>
+            <Button color="black" bgcolor="rgb(245,106,141)">S</Button>
+        </Spacer>
         <Background>
             <H>Total</H>
             <P>{`${price}SEK`}</P>
             <P>{`(includes ${tax}SEK Tax)`}</P>
             <Button color="black" bgcolor="rgb(245,106,141)">Checka ut med klarna</Button>
         </Background>
+        </>
     );
 }
 
