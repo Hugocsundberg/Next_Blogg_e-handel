73a55ab5d027944e2747538a77d456d521b2cca4
Hugo Sundberg <55212646+Hugocsundberg@users.noreply.github.com>
2021-08-01 00:30:00 +0200

    Add screen sizes style variables

--- styles/globalStyleVariables.ts
+++ styles/globalStyleVariables.ts
@@ -1,3 +1,5 @@
+import { ScreenSizes } from "../generalTypes"
+
 export const rem:number = 16
 export const darkGray:string = 'rgba(51, 51, 51, 1)'
 export const blurPx:number = 10
@@ -7,6 +9,10 @@ export const lineWidth:number = 1
 export const imageHeight:number = 1.7
 export const darken:string = 'rgba(0, 0, 0, 0.07)';
 export const lighten:string = 'rgba(255, 255, 255, 0.8)';
-export const screenSizes:Array<number> = [600, 900]
+export const screenSizes:ScreenSizes = {
+    S: 500,
+    M: 700,
+    L: 1000
+}
 export const navHeight:number = 5.375
 export const buttonBorderRadius:number = 5
