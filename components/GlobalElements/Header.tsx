7cb0e03a57cc550e3b64f7f266f3953de9a4acbf
Hugo Sundberg <55212646+Hugocsundberg@users.noreply.github.com>
2021-08-01 20:35:34 +0200

    Alter Header element

--- components/GlobalElements/Header.tsx
+++ components/GlobalElements/Header.tsx
@@ -2,14 +2,13 @@
 import styled from 'styled-components'
 import { darkGray, margin } from '../../styles/globalStyleVariables'
 
-const Header = styled.h1`
-    /* color: ${darkGray}; */
+const Header = styled.h1<{noLeftMargin?: boolean}>`
     color: ${darkGray};
     font-weight: bold;
     font-size: 2.3rem;
     display: flex;
     justify-content: start;
-    padding: ${margin}rem ${margin}rem;
+    padding: ${props => props.noLeftMargin ? `${margin}rem 0rem` : `${margin}rem ${margin}rem`};
     margin: 0;
     align-items: center;
 `
