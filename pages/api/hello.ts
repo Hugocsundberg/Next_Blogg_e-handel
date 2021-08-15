1609f9beb3599fc4ec30cec4138a5254c3ce8b38
Hugo Sundberg <55212646+Hugocsundberg@users.noreply.github.com>
2021-08-15 21:20:15 +0200

    Fix Klarna integration

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,5 +1,46 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { Product } from '../../generalTypes';
+const fetch = require('node-fetch');
+const btoa = require('btoa');
+require('dotenv').config()
 
-export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
+export default (req: NextApiRequest, res: NextApiResponse) => {
+  const productArray:Array<Product> = req.body
+  const klarnaBody = {
+          locale: "sv-se",
+          purchase_country: "SE",
+          purchase_currency: "SEK",
+          order_amount: productArray.reduce((acc, cur) => acc + cur.price, 0) * 100,
+          order_tax_amount: 0,
+          order_lines: req.body.map((product:Product)=>{
+            return {
+              name: `${product.title}`,
+              quantity: 1,
+              unit_price: product.price * 100,
+              tax_rate: 0,
+              total_amount: product.price * 100,
+              total_tax_amount: 0
+            }
+          }),
+          merchant_urls: {
+            terms: `${process.env.BASE_URL}/terms`, 
+            checkout: `${process.env.BASE_URL}/cart`,
+            confirmation: `${process.env.BASE_URL}/confirmation?order_id={checkout.order.id}`,
+            push: `${process.env.BASE_URL}/api/push?order_id={checkout.order.id}`
+        }
+      }
+  
+     const klarnaOptions = {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Basic ${btoa(process.env.KLARNA_AUTH)}`
+        },
+        body: JSON.stringify(klarnaBody),
+      };
+      fetch('https://api.playground.klarna.com/checkout/v3/orders', klarnaOptions)
+      .then((stream:any)=>stream.json())
+      .then((data:any)=>{
+        res.json({htmlSnippet: data.html_snippet})
+      })
 }
