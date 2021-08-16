2fcd41172a75ce9ef3fd4c58ffaa79d853d7b082
Hugo Sundberg <55212646+Hugocsundberg@users.noreply.github.com>
2021-08-16 11:26:14 +0200

    Add nav to confirmation page

--- pages/confirmation.tsx
+++ pages/confirmation.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
 import router from 'next/router';
+import { AboutMe } from '../generalTypes';
+import Head from "next/head"
+import client from '../client';
+import Nav from '../components/Nav';
+import { useEffect, useState } from 'react';
+import { getTopOverlayHeight } from '../functions';
+import { Background } from '../components/GlobalElements';
 
-const OuterFlex = styled.div`
+const OuterFlex = styled.div<{topOverlayHeight:number}>`
     display: flex;
     justify-content: center;
     align-items: center;
-    height: 100vh;
+    height: calc(100vh - ${props=>props.topOverlayHeight}px);
+    overflow: hidden;
     width: 100vw;
     flex-direction: column;
     gap: 2rem;
@@ -31,16 +39,48 @@ const P = styled.p`
     text-align: center;
 `
 
-const confirmation = () => {
+const confirmation = ({ aboutMe }: {aboutMe: string}) => {
+    const _aboutMe:Array<AboutMe> = JSON.parse(aboutMe)
+    const [topOverlayHeight, settopOverlayHeight] = useState(0);
+    useEffect(()=>{
+        settopOverlayHeight(getTopOverlayHeight())
+        //set body scroll
+        document.body.style.overflow="hidden"
+        return () => {
+            document.body.style.overflow="initial"
+        }
+    }, [])
+   
     return (
-        <OuterFlex>
-            <img src="/success.svg" alt="success check mark icon" />
-            <RightFlex>
-                <H1>Din order har mottagits</H1>
-                <P>order id: {process.browser ? router.query.order_id : '-'}</P>
-            </RightFlex>
-        </OuterFlex>
+        <>  
+            <Head>
+                <title>Confirmation</title>
+            </Head>
+                <Nav aboutMe={_aboutMe}></Nav>
+                <OuterFlex topOverlayHeight={topOverlayHeight}>
+                    <img src="/success.svg" alt="success check mark icon" />
+                    <RightFlex>
+                        <H1>Din order har mottagits</H1>
+                        <P>order id: {process.browser ? router.query.order_id : '-'}</P>
+                    </RightFlex>
+                </OuterFlex>
+        </>
     );
 }
+
+export async function getStaticProps() {
+    let settingsData
+    const settingsquery = '*[_type == "settings"]{"slug": aboutme->slug,"title": aboutme->title}'
+    await client.fetch(settingsquery)
+    .then((settings: Array<AboutMe>) => settingsData = settings)
+    const settingsJson = JSON.stringify(settingsData)
+  
+    return {
+      props: {
+        aboutMe: settingsJson
+      },
+      revalidate: 60
+    }
+  }
 
 export default confirmation;
