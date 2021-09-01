b1cf1b8213e476815231df0cd21570bd6c5c04f5
Hugo Sundberg <55212646+Hugocsundberg@users.noreply.github.com>
2021-09-01 17:11:34 +0200

    Add data change observation for reservations

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -6,11 +6,59 @@ import Nav from '../../components/Nav'
 import { screenSizes } from '../../styles/globalStyleVariables'
 import Head from "next/head"
 import { Header } from "../../components/GlobalElements"
+import { useEffect, useState } from "react"
 
-const Products = ({ products, aboutMe }: {products: string, aboutMe: string}) => {
+const Products = ({ aboutMe }: { aboutMe: string}) => {
+  let suscriptionHasRun:boolean = false;
   const _aboutMe:Array<AboutMe> = JSON.parse(aboutMe)
+  const [_products, set_products] = useState<Array<ProductType>>([]);
+  const productsQuery = `*[_type == 'product']{_createdAt, productHeight, "id": _id, lastReservedAt, productWidth, productDept, _updatedAt, slug, "alt":image.alt, "images": images[]{asset, alt, 'Asset':asset->, "imageHeight": asset->metadata.dimensions.height, "imageWidth": asset->metadata.dimensions.width}, price, desc, title, "imageHeight": metadata.dimensions.height, "imageWidth": image.asset->metadata.dimensions.width}`
 
-    const _products: Array<ProductType> = JSON.parse(products)
+  useEffect(()=>{
+    if(process.browser) {
+      
+      client.fetch(productsQuery)
+      .then((data:Array<ProductType>)=> {
+        console.log(data)
+        set_products([...data])
+      })
+    }
+  }, [])
+
+  useEffect(()=>{
+    if(!suscriptionHasRun) {
+      suscriptionHasRun = true
+      const subscription = client.listen(productsQuery, {}, {includeResult: true})
+        .subscribe((update:any) => {
+          let object:ProductType = update.result
+          const productArray = [..._products]
+  
+          let updatePosition:number = 0;
+          
+          for(let i:number = 0; i < productArray.length; i++) {
+            if(productArray[i].slug.current === object.slug.current) {
+              updatePosition = i
+              break
+            }
+          }
+
+          setTimeout(() => {
+            client.fetch(`*[_type == 'product' && slug.current == '${object.slug.current}']{_createdAt, lastReservedAt, productHeight, productWidth, productDept, sold, pending, _updatedAt, slug, "alt":image.alt, "images": images[]{asset, alt, "imageHeight": asset->metadata.dimensions.height, "imageWidth": asset->metadata.dimensions.width}, price, desc, title, "imageHeight": metadata.dimensions.height, "imageWidth": image.asset->metadata.dimensions.width}`)
+            .then((data:Array<ProductType>)=>{
+              object = data[0]
+              productArray.splice(updatePosition, 1, object)
+              set_products([...productArray])
+            })
+          }, 2500);
+          
+        })  
+  
+        return () => {
+          subscription.unsubscribe()
+        }
+    }
+  }, [_products])
+
     const breakpointColumnsObj = {
       default: 4,
       [screenSizes.L]: 3,
@@ -29,8 +77,8 @@ const Products = ({ products, aboutMe }: {products: string, aboutMe: string}) =>
           breakpointCols={breakpointColumnsObj}
           className="masonry-grid"
           columnClassName="masonry-grid_column">
-          {_products.map((product, key)=>(
-            <Product key={key} alt={product.alt || 'no alt text'} images={product.images} slug={product.slug.current} hasShadow={true}></Product>
+          {_products.map((product:ProductType, key:any)=>(
+            <Product sold={product.sold} lastReserved={product.lastReservedAt} key={key} alt={product.alt || 'no alt text'} images={product.images} slug={product.slug.current} hasShadow={true}></Product>
           ))}
         </Masonry>
       </>
@@ -38,12 +86,12 @@ const Products = ({ products, aboutMe }: {products: string, aboutMe: string}) =>
   }
 
 export async function getStaticProps() {
-    const query = `*[_type == 'product']{_createdAt, productHeight, "id": _id, lastReservedAt, productWidth, productDept, _updatedAt, slug, "alt":image.alt, "images": images[]{asset, alt, 'Asset':asset->, "imageHeight": asset->metadata.dimensions.height, "imageWidth": asset->metadata.dimensions.width}, price, desc, title, "imageHeight": metadata.dimensions.height, "imageWidth": image.asset->metadata.dimensions.width}`
-    let productsData
-    await client.fetch(query)
-    .then((products: any) => productsData = products)
-     let productJson: string
-     productsData ? productJson = JSON.stringify(productsData) : productJson = '{}'
+    // const query = `*[_type == 'product']{_createdAt, productHeight, "id": _id, lastReservedAt, productWidth, productDept, _updatedAt, slug, "alt":image.alt, "images": images[]{asset, alt, 'Asset':asset->, "imageHeight": asset->metadata.dimensions.height, "imageWidth": asset->metadata.dimensions.width}, price, desc, title, "imageHeight": metadata.dimensions.height, "imageWidth": image.asset->metadata.dimensions.width}`
+    // let productsData
+    // await client.fetch(query)
+    // .then((products: any) => productsData = products)
+    //  let productJson: string
+    //  productsData ? productJson = JSON.stringify(productsData) : productJson = '{}'
 
     let settingsData
     const settingsquery = '*[_type == "settings"]{"slug": aboutme->slug,"title": aboutme->title}'
@@ -53,7 +101,7 @@ export async function getStaticProps() {
   
     return {
       props: {
-        products: productJson,
+        // products: productJson,
         aboutMe: settingsJson
       },
       revalidate: 10
