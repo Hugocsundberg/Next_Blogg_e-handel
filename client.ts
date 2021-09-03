2f94ca516a0e706a4c2910c8215ddb7d7da6eb86
Hugo Sundberg <55212646+Hugocsundberg@users.noreply.github.com>
2021-09-03 19:14:30 +0200

    Use cdn false

--- client.ts
+++ client.ts
@@ -4,6 +4,6 @@ const client = sanityClient({
   dataset: 'production',
   apiVersion: '2021-06-13', // use current UTC date - see "specifying API version"!
   token: '', // or leave blank for unauthenticated usage
-  useCdn: true, // `false` if you want to ensure fresh data
+  useCdn: false, // `false` if you want to ensure fresh data
 })
 export default client
