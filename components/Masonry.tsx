83825caa19ee800b1cbb815a9cd30700c36850d1
Hugo Sundberg <55212646+Hugocsundberg@users.noreply.github.com>
2021-10-26 13:39:15 +0200

    Bug fixes to masonry component

--- components/Masonry.tsx
+++ components/Masonry.tsx
@@ -2,6 +2,7 @@ import React, { JSXElementConstructor } from 'react';
 import styled from 'styled-components';
 import { breakPoints } from '../generalTypes';
 import { useState, useEffect } from 'react';
+import { Product as ProductType } from "../generalTypes"
 
 const Cols = styled.div<({columns:number})>`
     display: flex;
@@ -12,27 +13,12 @@ const Cols = styled.div<({columns:number})>`
 
 const Col = styled.div`
     width: 100%;
-`
+    `
 
-const Masonry = ({ breakPoints, children }: {breakPoints: breakPoints, children:Array<Element>}, ref:any) => {
-    const [colCount, setColCount] = useState(1);
-    const [cols, setCols] = useState<Array<Array<Element>>>([]);
-
-    useEffect(() => {
-        const _cols:Array<Array<Element>> = []
-        for(let i = 0; i < colCount; i++) {
-            _cols.push([])
-        }
-
-        for(let i = 0; i < children.length; i++) {
-            _cols[i % colCount].push(children[i])
-        }
-        setCols(_cols)
-    }, [colCount]);
-
-    useEffect(() => {
-        console.log({cols})
-    }, [cols]);
+const Masonry = ({ breakPoints, children, result, skeleton }: {breakPoints: breakPoints, children:Array<Array<JSX.Element>>, result:Array<ProductType>, skeleton:Array<Object> }, ref:any) => {
+    
+    const [colCount, setColCount] = useState(3);
+    const [cols, setCols] = useState<Array<Array<React.ReactNode>>>([]);
     
     const updateColCount = () => {
         const width = window.innerWidth
@@ -41,14 +27,37 @@ const Masonry = ({ breakPoints, children }: {breakPoints: breakPoints, children:
         else if(width < breakPoints.L) setColCount(3)  
         else setColCount(4) 
     }
+    
+    // Local '_children variable used to merge child arrays into single array'
+    const _children:Array<any> = [];
+    for(let i = 0; i < children.length; i++) {
+        _children.push(...children[i]);
+    }
 
+    // Set columns whenever new chilfren are added or colcount changes.
     useEffect(() => {
-        updateColCount()
+        const _cols:Array<Array<React.ReactNode>> = []
+        // Add new columns 
+        for(let i = 0; i < colCount; i++) {
+            _cols.push([])
+        }
+        // Fill columns 
+        for(let i = 0; i < _children.length; i++) {
+            _cols[i % colCount].push(_children[i])
+        }
+        setCols(_cols)
+    }, [colCount, result, skeleton]);
+    
+    useEffect(() => {
         window.addEventListener('resize', updateColCount)
         return () => {
             window.removeEventListener('resize', updateColCount)
         }
     }, []);
+
+    useEffect(()=>{
+        updateColCount()
+    })
 
     return (
         <Cols ref={ref} columns={colCount}>
