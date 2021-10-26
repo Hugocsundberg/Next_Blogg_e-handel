0dec3167f4e56a90f75a28ca054c45f10815c61a
Hugo Sundberg <55212646+Hugocsundberg@users.noreply.github.com>
2021-10-26 13:55:11 +0200

    Fix lazy load hook

--- hooks/useLazyLoad.ts
+++ hooks/useLazyLoad.ts
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import client from "../client";
 
-const useLazyLoad = <resultType>(query:string) => {
+const useLazyLoad = <resultType>(query:string, incrementBy: number) => {
     const [result, setResult] = useState<Array<resultType>>([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState<Error>();
@@ -12,9 +12,16 @@ const useLazyLoad = <resultType>(query:string) => {
             setLoading(true)
             client.fetch(query)
             .then((data:Array<resultType>) => {
-                setHasMore(data.length !== 0)
+                // IncrementBy is currently one bigger than the number we want so that we can check if there are more. Can probably get better by using tagged template string. 
+                if(data.length >= incrementBy) {
+                    setHasMore(true)
+                    // pops the last check=item
+                    data.pop()
+                } else {
+                    setHasMore(false)
+                }
                 setResult(prevData => [...prevData, ...data])
-                setLoading(true)
+                setLoading(false)
             }).catch((e:Error)=>setError(e))
         }
     }, [query])
