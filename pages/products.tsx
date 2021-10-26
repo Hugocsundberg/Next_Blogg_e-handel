96610a5ca1e0ee20b027cc8de8750e4a10dd15d3
Hugo Sundberg <55212646+Hugocsundberg@users.noreply.github.com>
2021-10-26 14:12:57 +0200

    Redo lazy loading

--- pages/products.tsx
+++ pages/products.tsx
@@ -1,101 +1,167 @@
 import client from "../client"
 import { AboutMe, Product as ProductType } from "../generalTypes"
-import Masonry from 'react-masonry-css'
+// import Masonry from 'react-masonry-css'
 import Product from "../components/Product"
 import Nav from '../components/Nav'
 import { screenSizes } from '../styles/globalStyleVariables'
 import Head from "next/head"
 import { Header } from "../components/GlobalElements"
-import { useEffect, useState } from "react"
+import { useCallback, useEffect, useRef, useState } from "react"
+import useLazyLoad from "../hooks/useLazyLoad"
+import Skeleton from "../components/Skeleton"
+import { motion } from "framer-motion"
+import React from "react"
+import Masonry from "../components/Masonry"
+
+const MasonryComponent = React.forwardRef(Masonry)
+const MotionMasonry = motion(MasonryComponent)
+
+const SkeletonComponent = React.forwardRef(Skeleton)
+const MotionSkeleton = motion(SkeletonComponent)
 
 const Products = ({ aboutMe }: { aboutMe: string}) => {
-  let suscriptionHasRun:boolean = false;
   const _aboutMe:Array<AboutMe> = JSON.parse(aboutMe)
-  const [_products, set_products] = useState<Array<ProductType>>([]);
-  const productsQuery = `*[_type == 'product']{_createdAt, productHeight, "id": _id, lastReservedAt, sold, productWidth, productDept, _updatedAt, slug, "alt":image.alt, "images": images[]{asset, alt, 'Asset':asset->, "imageHeight": asset->metadata.dimensions.height, "imageWidth": asset->metadata.dimensions.width}, price, desc, title, "imageHeight": metadata.dimensions.height, "imageWidth": image.asset->metadata.dimensions.width}`
+  const incrementBy = 4
+  const [query, setQuery] = useState(`*[_type == 'product']{_createdAt, productHeight, "id": _id, lastReservedAt, sold, productWidth, productDept, _updatedAt, slug, "alt":image.alt, "images": images[]{asset, alt, 'Asset':asset->, "imageHeight": asset->metadata.dimensions.height, "imageWidth": asset->metadata.dimensions.width}, price, desc, title, "imageHeight": metadata.dimensions.height, "imageWidth": image.asset->metadata.dimensions.width}[0...${incrementBy}]`);
+  const {result, loading, error, hasMore, setResult} = useLazyLoad<ProductType>(query, incrementBy)
+  const [skeletonArray, setSkeletonArray] = useState<Array<object>>([])
+  const [currentProduct, setCurrentProduct] = useState(0);
+  
+  const containerVariants = {
+    fade: {
+      transition: {
+          staggerChildren: 0.5
+      }
+  }, 
+  }
 
-  useEffect(()=>{
-    if(process.browser) {
-      client.fetch(productsQuery)
-      .then((data:Array<ProductType>)=> {
-        set_products([...data])
-      })
+  const itemVariants = {
+    fade: {
+      opacity: 0.2,
+      transition: {
+          repeat: Infinity
+      }
+  }, 
+  }
+
+useEffect(() => {
+  if(error) console.log(error)
+}, [error]);
+
+  const observableCallback = (update:any) => {
+        let object:ProductType = update.result
+        const productArray = [...result]
+
+        let updatePosition:number = 0;
+        
+        for(let i:number = 0; i < productArray.length; i++) {
+          if(productArray[i].slug.current === object.slug.current) {
+            updatePosition = i
+            break
+          }
+        }
+
+        setTimeout(() => {
+          client.fetch(`*[_type == 'product' && slug.current == '${object.slug.current}']{_createdAt, lastReservedAt, productHeight, productWidth, productDept, sold, pending, _updatedAt, slug, "alt":image.alt, "images": images[]{asset, alt, "imageHeight": asset->metadata.dimensions.height, "imageWidth": asset->metadata.dimensions.width}, price, desc, title, "imageHeight": metadata.dimensions.height, "imageWidth": image.asset->metadata.dimensions.width}`)
+          .then((data:Array<ProductType>)=>{
+              object = data[0]
+              const productArray = [...result]
+              productArray.splice(updatePosition, 1, object)
+              setResult(productArray)
+          })
+        }, 2500);
+
+      return () => {
+        observable.current.unsubscribe()
+      }
+  }
+
+  const observer = useRef<IntersectionObserver>()
+  const observable = useRef(client.listen(query, {}, {includeResult: true}))
+
+  useEffect(() => {
+    const subscription = observable.current.subscribe(observableCallback)
+    return () => {
+      subscription.unsubscribe()
     }
+  }, [result]);
+
+  const lastElementRef = useCallback(node=>{
+      if(observer.current) observer.current.disconnect()
+      observer.current = new IntersectionObserver((e)=>{
+        if(e[0].isIntersecting) {
+            setCurrentProduct(prevValue => prevValue + incrementBy - 1)
+        }
+      });
+      if(node) observer.current.observe(node)
   }, [])
 
+  useEffect(() => {
+    if(!hasMore) observer.current?.disconnect()
+  }, [hasMore]);
+
   useEffect(()=>{
-    if(!suscriptionHasRun) {
-      suscriptionHasRun = true
-      const subscription = client.listen(productsQuery, {}, {includeResult: true})
-        .subscribe((update:any) => {
-          let object:ProductType = update.result
-          const productArray = [..._products]
-  
-          let updatePosition:number = 0;
-          
-          for(let i:number = 0; i < productArray.length; i++) {
-            if(productArray[i].slug.current === object.slug.current) {
-              updatePosition = i
-              break
-            }
-          }
+    setQuery(`*[_type == 'product']{_createdAt, productHeight, "id": _id, lastReservedAt, sold, productWidth, productDept, _updatedAt, slug, "alt":image.alt, "images": images[]{asset, alt, 'Asset':asset->, "imageHeight": asset->metadata.dimensions.height, "imageWidth": asset->metadata.dimensions.width}, price, desc, title, "imageHeight": metadata.dimensions.height, "imageWidth": image.asset->metadata.dimensions.width}[${currentProduct}...${currentProduct + incrementBy}]`)
+  }, [currentProduct])
 
-          setTimeout(() => {
-            client.fetch(`*[_type == 'product' && slug.current == '${object.slug.current}']{_createdAt, lastReservedAt, productHeight, productWidth, productDept, sold, pending, _updatedAt, slug, "alt":image.alt, "images": images[]{asset, alt, "imageHeight": asset->metadata.dimensions.height, "imageWidth": asset->metadata.dimensions.width}, price, desc, title, "imageHeight": metadata.dimensions.height, "imageWidth": image.asset->metadata.dimensions.width}`)
-            .then((data:Array<ProductType>)=>{
-              object = data[0]
-              productArray.splice(updatePosition, 1, object)
-              set_products([...productArray])
-            })
-          }, 2500);
-        })  
-  
-        return () => {
-          subscription.unsubscribe()
-        }
+  useEffect(()=>{
+    if(loading && hasMore) {
+      let _skeletonArray = [];
+      for(let i = 0; i < incrementBy - 1; i++) {
+        _skeletonArray.push({})
+      }
+      setSkeletonArray(_skeletonArray)
     }
-  }, [_products])
-
-    const breakpointColumnsObj = {
-      default: 4,
-      [screenSizes.L]: 3,
-      [screenSizes.M]: 2,
-      [screenSizes.S]: 1
-    };
-    console.log(_products)
-    return (
-      <>
+    if(!loading) {
+      setSkeletonArray([])
+    }
+  }, [loading])
+  
+  const breakpointColumnsObj = {
+    S: screenSizes.S,
+    M: screenSizes.M,
+    L: screenSizes.L
+  };
+  
+  return (
+    <>
         <Head>
         <title>Products</title>
         </Head>
         <Nav aboutMe={_aboutMe}></Nav>
         <Header>PRODUKTER</Header>
-        <Masonry
-          breakpointCols={breakpointColumnsObj}
-          className="masonry-grid"
-          columnClassName="masonry-grid_column">
-          {_products.map((product:ProductType, key:any)=>(
-            <Product sold={product.sold} lastReserved={product.lastReservedAt} key={key} alt={product.alt || 'no alt text'} images={product.images} slug={product.slug.current} hasShadow={true}></Product>
-          ))}
-        </Masonry>
+        <MotionMasonry
+          variants={containerVariants}
+          animate={'fade'}
+          breakPoints={breakpointColumnsObj}
+          result={result}
+          skeleton={skeletonArray}
+          > 
+          {result.map((product:ProductType, i)=>{
+            
+            if(i + 1 >= result.length) 
+            return <Product lastElementRef={lastElementRef} sold={product.sold} lastReserved={product.lastReservedAt} key={i} alt={product.alt || 'no alt text'} images={product.images} slug={product.slug.current} hasShadow={true}></Product>
+            
+            else 
+            return <Product sold={product.sold} lastReserved={product.lastReservedAt} key={i} alt={product.alt || 'no alt text'} images={product.images} slug={product.slug.current} hasShadow={true}></Product>
+          })
+        }
+        {skeletonArray.map(()=>(
+        <Skeleton></Skeleton>
+        ))}
+        </MotionMasonry>
       </>
     ) 
   }
-
-export async function getStaticProps() {
-    // const query = `*[_type == 'product']{_createdAt, productHeight, "id": _id, lastReservedAt, productWidth, productDept, _updatedAt, slug, "alt":image.alt, "images": images[]{asset, alt, 'Asset':asset->, "imageHeight": asset->metadata.dimensions.height, "imageWidth": asset->metadata.dimensions.width}, price, desc, title, "imageHeight": metadata.dimensions.height, "imageWidth": image.asset->metadata.dimensions.width}`
-    // let productsData
-    // await client.fetch(query)
-    // .then((products: any) => productsData = products)
-    //  let productJson: string
-    //  productsData ? productJson = JSON.stringify(productsData) : productJson = '{}'
-
+  
+  export async function getStaticProps() {
+    
     let settingsData
     const settingsquery = '*[_type == "settings"]{"slug": aboutme->slug,"title": aboutme->title}'
     await client.fetch(settingsquery)
     .then((settings: Array<AboutMe>) => settingsData = settings)
     const settingsJson = JSON.stringify(settingsData)
-  
+    
     return {
       props: {
         // products: productJson,
@@ -104,5 +170,5 @@ export async function getStaticProps() {
       revalidate: 10
     }
   }
-
+  
   export default Products
