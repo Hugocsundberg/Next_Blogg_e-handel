92c8905fc3f0ab1ad19f8cc66533c9ef3bcc194e
Hugo Sundberg <55212646+Hugocsundberg@users.noreply.github.com>
2021-11-01 15:12:03 +0100

    Add nothingInCart component

---
+++ components/CartComponents/NothingInCart.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import styled from 'styled-components';
+import Image from 'next/dist/client/image';
+import { boxShadowBigElement } from '../../styles/globalStyleVariables';
+import Link from 'next/dist/client/link';
+
+const Background = styled.div`
+    background: white;
+    opacity: 0.78;
+    border-radius: 10px;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    flex-direction: column;
+    box-shadow: ${boxShadowBigElement};
+`
+
+const H1 = styled.h1`
+`
+
+const P = styled.p`
+`
+
+const Span = styled.span`
+    text-decoration: underline;
+    font-weight: bold;
+    cursor: pointer;
+`
+
+const ImageContainer = styled.div`
+    height: 200px;
+    width: 100%;
+    position: relative;
+`
+
+const NothingInCart = () => {
+    return (
+        <Background>
+            <H1>Här var det tomt</H1>
+            <ImageContainer>
+                <Image
+                    src="/sadCart.png"
+                    alt="hamburger"
+                    layout="fill"
+                    objectFit="contain"
+                />
+            </ImageContainer>
+            <Link href='/tavlor'><P>Lägg till <Span>tavlor</Span> så kommer de synas här</P></Link>
+        </Background>
+    );
+}
+
+export default NothingInCart;
