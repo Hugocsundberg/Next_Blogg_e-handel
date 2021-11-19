e1d1a56e1cc3d10afc1699c6ad29bb4a65b6bee2
Hugo Sundberg <55212646+Hugocsundberg@users.noreply.github.com>
2021-11-19 16:01:56 +0100

    add cart animation

--- components/Nav/cartIcon.tsx
+++ components/Nav/cartIcon.tsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import Image from "next/dist/client/image";
 import { imageHeight } from "../../styles/globalStyleVariables";
+import {  motion } from "framer-motion";
+
 
 const Cart = styled.div`
     height: ${imageHeight}rem;
@@ -20,18 +22,42 @@ const Cart = styled.div`
 const CartP = styled.p`
     font-size: 0.7rem;
     transform: translate(2px, -1px);
-`
+    `
 
 export const CartIcon = ({cartItems}: {cartItems: number | string}, ref:any) => {
+    const [animation, setAnimation] = useState(false)
+    
+    const variants = {
+        inactive: {},
+        active: {
+            y: [0, -10, 0],
+            transition: { stiffness: 1, mass: 100 }
+        }
+    }
+
+
+    const triggerAnimation = () => {
+            setAnimation(true)
+            setTimeout(() => {
+                setAnimation(false)
+            }, 2000);
+    }
+    
+    useEffect(()=>{
+        window.addEventListener('updatecart', triggerAnimation)
+        return () => window.removeEventListener('updatecart', triggerAnimation)
+    }, [])
+
+    
     return (
-        <Cart ref={ref}>
+        <Cart as={motion.div} animate={animation ? "active" : "inactive"} variants={variants} ref={ref}>
             <CartP>{cartItems}</CartP>
             <Image
                 src="/shop-cart.svg"
                 alt="hamburger"
                 layout="fill"
                 objectFit="contain"
-            />
+                />
         </Cart>
     )
 }
