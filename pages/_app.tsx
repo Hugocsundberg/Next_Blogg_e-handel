3a4ba7e6b1cfdb79e798f2dcf190e656380425b6
Hugo Sundberg <55212646+Hugocsundberg@users.noreply.github.com>
2021-11-22 18:34:00 +0100

    remove comment

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import "../styles/globals.css";
 import React from "react";
-// import App from "next/app";
 import type { AppProps /*, AppContext */ } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
