27dfdffef126f8cfc8a002f285b82ab85c3fe995
Hugo Sundberg <55212646+Hugocsundberg@users.noreply.github.com>
2022-05-07 13:59:50 +0200

    update gtag manager

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -22,23 +22,6 @@ export default class MyDocument extends Document {
           <>
             {initialProps.styles}
             {sheet.getStyleElement()}
-            <Head>
-              <script
-                async
-                src="https://www.googletagmanager.com/gtag/js?id=G-PMZJ3N9JGY"
-              />
-
-              <script
-                dangerouslySetInnerHTML={{
-                  __html: `
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-                    gtag('config', 'G-PMZJ3N9JGY', { page_path: window.location.pathname });
-                  `,
-                }}
-              />
-            </Head>
           </>
         ),
       };
